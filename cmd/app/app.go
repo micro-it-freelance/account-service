6effcaa38e6a9310b65ca5c366c67ee213571a42
iivkis <razmolodinivan@mail.ru>
2023-03-19 12:36:18 +0700

    add migrations

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -14,8 +14,6 @@ import (
 )
 
 func main() {
-	
-
 	// connect to database
 	db, err := sqlx.Connect("pgx", fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%d sslmode=disable",
 		config.DB.Name, config.DB.User, config.DB.Password, config.DB.Host, config.DB.Port))
