cdd497682ad52cd87461b4c0501db06012e0f9f6
iivkis <razmolodinivan@mail.ru>
2023-04-30 14:19:02 +0700

    implement entity

--- internal/app/account/entity.go
+++ internal/app/account/entity.go
@@ -1,10 +1,16 @@
 package account
 
-import "time"
+import (
+	"time"
+
+	core_entity "github.com/micro-it-freelance/core/entity"
+)
 
 type Account struct {
 	ID         int64     `db:"id" json:"db"`
 	TelegramID int64     `db:"telegram_id" json:"telegram_id"`
 	Username   string    `db:"username" json:"username"`
 	CreatedAt  time.Time `db:"created_at" json:"created_at"`
+
+	core_entity.ImplementedEntity
 }
